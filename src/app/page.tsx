91e32a4712c5c075027f3a235bb6c5892fca6514
DinhNT <compimprove@gmail.com>
2024-07-15 16:13:38 +0700

    doing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,20 @@ import { Calendar as CalendarIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+import { DatetimePicker } from "@/components/ui/datetime-picker";
 import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 export default function Home() {
   const [date, setDate] = React.useState<Date>(new Date());
 
   return (
     <main className="flex mt-40 flex-col items-center justify-center">
-      <Dialog>
-        <DialogTrigger asChild>
+      <Popover>
+        <PopoverTrigger asChild>
           <Button
             variant={"outline"}
             className={cn(
@@ -30,16 +28,13 @@ export default function Home() {
             )}
           >
             <CalendarIcon className="mr-2 h-4 w-4" />
-            {date ? format(date, "PPP - HH:mm") : <span>Pick a date</span>}
+            {date ? format(date, "PPP hh:mm") : <span>Pick a date</span>}
           </Button>
-        </DialogTrigger>
-        <DialogContent className="w-auto rounded-lg gap-0">
-          <DialogHeader>
-            <DialogTitle className="text-center">Pick your date</DialogTitle>
-          </DialogHeader>
-          <Calendar selected={date} setDate={setDate} initialFocus />
-        </DialogContent>
-      </Dialog>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-4">
+          <DatetimePicker selected={date} setDate={setDate} initialFocus />
+        </PopoverContent>
+      </Popover>
     </main>
   );
 }
